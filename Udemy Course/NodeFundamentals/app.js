d160aa55d1c94581ed981f21302c6177c3010199
Franco Sebastian Arraño Silva <franco@MacBook-Pro-de-Franco.local>
2017-09-15 15:44:32 -0300

    Se agrega otra clase donde se trabaja con librerias externas

--- Udemy Course/NodeFundamentals/app.js
+++ Udemy Course/NodeFundamentals/app.js
@@ -2,10 +2,16 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash')
 const notes = require('./notes.js');
 
 console.log('Result:', notes.add(9,-2));
 
+//console.log(_.isString(true));
+//console.log(_.isString("Franco"));
+
+var filteredArray = _.uniq(['Andrew',1,'Andrew',1,2,3,4]);
+console.log(filteredArray);
 /*
 var user = os.userInfo();
 
