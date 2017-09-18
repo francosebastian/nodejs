ab589fa478354f959e79313d98abefb013503fad
Franco Sebastian Arraño Silva <franco@MacBook-Pro-de-Franco.local>
2017-09-18 11:01:36 -0300

    Manejo de argumentos de consola con yargs

--- Udemy Course/NodeFundamentals/app.js
+++ Udemy Course/NodeFundamentals/app.js
@@ -1,24 +1,24 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
-const _ = require('lodash')
+const _ = require('lodash');
+const yargs = require('yargs')
+
 const notes = require('./notes.js');
 
-console.log('Result:', notes.add(9,-2));
+const argv = yargs.argv;
+var command = argv._[0];
+console.log("Command ", command);
+console.log('Yargs ',argv);
 
-//console.log(_.isString(true));
-//console.log(_.isString("Franco"));
-
-var filteredArray = _.uniq(['Andrew',1,'Andrew',1,2,3,4]);
-console.log(filteredArray);
-/*
-var user = os.userInfo();
-
-
-fs.appendFile('greetings.txt',`Hello ${user.username}! You are ${notes.age}`, function(err){
-    if(err){
-        console.log('unable to write to file')
-    }
-});
-*/
+if (command === 'add') {
+    notes.addNote(argv.title, argv.body);
+} else if (command === 'list') {
+    notes.getAll();
+} else if (command === 'read') {
+    notes.getNote(argv.title);
+} else if (command === 'remove') {
+    notes.removeNote(argv.title);
+} else {
+    console.log('Not recognize');
+}
--- Udemy Course/NodeFundamentals/notes.js
+++ Udemy Course/NodeFundamentals/notes.js
@@ -1,12 +1,25 @@
 console.log('Starting notes.js');
 
-module.exports.age = 25;
-
-module.exports.addNote = () => {
-  console.log('addNote');
-  return `New note`
+var addNote = (title, body) => {
+    console.log('Adding note', title, body)
 };
 
-module.exports.add = (a,b) => {
-    return a+b;
+var getAll = () => {
+    console.log('Getting all notes');
 };
+
+var getNote = (title) => {
+  console.log('Getting note',title);
+};
+
+var removeNote = () => (title) => {
+    console.log('Removing note',title);
+};
+
+module.exports = {
+    addNote,
+    getAll,
+    getNote,
+    removeNote
+};
+
